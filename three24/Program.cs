55aa76cd3cada35aa9865eaaf7fcd4e05ab5ccfd
Лера Голикова <funtik-98@mail.ru>
2023-05-24 12:43:45 +0700

    Добавьте файлы проекта.

---
+++ three24/Program.cs
@@ -0,0 +1,58 @@
+﻿using three24;
+
+class Program
+{
+    static void Main()
+    {
+        // Создание дерева двоичного поиска
+        TreeNode root = null;
+
+        // Добавление слов в дерево по очереди
+        AddWord(ref root, "собака");
+        AddWord(ref root, "як");
+        AddWord(ref root, "кит");
+        AddWord(ref root, "слон");
+        AddWord(ref root, "кот");
+        AddWord(ref root, "белка");
+
+        // Обход дерева в прямом порядке и вывод на экран
+        Console.WriteLine("Результат обхода в прямом порядке:");
+        PreOrderTraversal(root);
+    }
+
+    // Метод для добавления слова в дерево
+    static void AddWord(ref TreeNode node, string word)
+    {
+        if (node == null)
+        {
+            // Если узел пустой, создаем новый узел с заданным значением
+            node = new TreeNode(word);
+        }
+        else if (string.Compare(word, node.Value) < 0)
+        {
+            // Если значение меньше значения текущего узла, рекурсивно добавляем в левое поддерево
+            AddWord(ref node.Left, word);
+        }
+        else
+        {
+            // Если значение больше или равно значению текущего узла, рекурсивно добавляем в правое поддерево
+            AddWord(ref node.Right, word);
+        }
+    }
+
+    // Метод для обхода дерева в прямом порядке
+    static void PreOrderTraversal(TreeNode node)
+    {
+        if (node != null)
+        {
+            // Выводим значение текущего узла
+            Console.WriteLine(node.Value);
+
+            // Рекурсивно обходим левое поддерево
+            PreOrderTraversal(node.Left);
+
+            // Рекурсивно обходим правое поддерево
+            PreOrderTraversal(node.Right);
+        }
+    }
+}
---
+++ three24/TreeNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace three24
+{
+    internal class TreeNode
+    {
+        public string Value;     // Значение узла
+        public TreeNode Left;    // Левый потомок
+        public TreeNode Right;   // Правый потомок
+
+        public TreeNode(string value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+    
+}
+}
